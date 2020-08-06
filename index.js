e39ee90887c0f25b1884b0ef351252c0a9f35ec3
unknown <djleonardhd@hotmail.com>
2020-08-06 00:35:36 -0300

    added typingStart

--- index.js
+++ index.js
@@ -35,5 +35,13 @@ client.on("messageCreate", (message) => {
         client.createMessage(message.channel.id, "Pong!");
     }
 });
+// typingStart
+/* Emitted whenever a user starts typing in a channel.
+PARAMETER      TYPE            DESCRIPTION
+channel        Channel         The channel the user started typing in
+user           User            The user that started typing    */
+client.on("typingStart", (channel, user) => {
+    console.log(`${user.tag} has started typing`);
+});
 
 client.connect();
