23d66371ee5300695ecbf6d6eb7ccac6ebaaadfb
unknown <djleonardhd@hotmail.com>
2020-08-06 00:09:14 -0300

    added shardPreReady

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const { Client } = require("eris");
 const client = new Client("BOT_TOKEN");
 
-// shard ready
+// shardPreReady
+/* Emitted when the fragment finishes processing the ready packet. */
+client.on("shardPreReady", (shardId) => {
+    console.log(`Initializing shard ${shardId}`);
+});
+// shardReady
 /* Emitted when the shard becomes ready to start working. */
 client.on("shardReady", (shardId) => {
-    console.log(`Shard ${shardId} initialized with sucess!`)
-})
+    console.log(`Shard ${shardId} initialized with sucess!`);
+});
 // ready
 /* Emitted when the client becomes ready to start working. */
 client.on("ready", () => {
     console.log("All the shards was initialized with sucess");
 });
 
-// message
+// messageCreate
 /* Emitted whenever a message is created.
 PARAMETER      TYPE           DESCRIPTION
 message        Message        The created message    */
